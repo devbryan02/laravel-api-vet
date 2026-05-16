7698443e1e0769412dad954c9b9a4356674d1e67
bryancmy <brayan7br7@gmail.com>
2026-05-16 15:54:51 -0500

    refactor: add relation and table name

--- app/Features/Vaccine/Models/Vaccine.php
+++ app/Features/Vaccine/Models/Vaccine.php
@@ -2,14 +2,17 @@
 
 namespace App\Features\Vaccine\Models;
 
+use App\Features\Pet\Models\Pet;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vaccine extends Model
 {
     use HasUlids;
 
-    
+    protected $table = 'vaccines';
+
     protected $fillable = [
         "type",
         "aplication_date",
@@ -18,5 +21,10 @@ class Vaccine extends Model
         "next_vaccine_date",
         "pet_id"
     ];
+
+    public function pet(): BelongsTo
+    {
+        return $this->belongsTo(Pet::class);
+    }
 
 }
